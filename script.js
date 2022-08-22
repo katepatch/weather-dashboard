d42c630ea6e031dbae2c41ae3a77d4bea221c7ce
kate patch <kate.epatch@gmail.com>
2022-08-22 16:58:56 -0600

    added function for getting item on script

--- script.js
+++ script.js
@@ -1,36 +1,19 @@
-$(document).ready(function(){
+var searchHistory = [];
 
+function getItems() {
+    var storedCities = JSON.parse(localStorage.getItem("searchHistory"));
+    if (storedCities !== null) {
+        searchHistory = storedCities;
+    };
 
+    for (i = 0; i < searchHistory.length; i++) {
+        if (i === 10) {
+            break;
+        }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-})
+        cityListButton = $("<a>").attr({
+            class: "city-list-item",
+            href: "#"
+        });
+    }
+}
